e62e8b15f8e2b4601c9afba1ddced40f328aa08d
Pau Andrio <andriopau@gmail.com>
2019-03-11 20:21:48 +0100

    Updating install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 #Download and install miniconda in the path
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
 bash $HOME/miniconda.sh -b -p $HOME/miniconda
-CONDA="$HOME/miniconda/bion/conda"
+CONDA="$HOME/miniconda/bin/conda"
 export PATH="$HOME/miniconda/bin:$PATH"
 
 $CONDA config --add channels defaults
 $CONDA config --add channels bioconda
 $CONDA config --add channels conda-forge
 
-$CONDA install -y biobb_wf_mutations
+$CONDA install -y jupyter
 
 $CONDA install -y nglview -c conda-forge
 jupyter-nbextension enable nglview --py --sys-prefix
 
-jupyter-notebook $HOME/mutations/mutations.ipynb
+$CONDA install -y biobb_wf_mutations
